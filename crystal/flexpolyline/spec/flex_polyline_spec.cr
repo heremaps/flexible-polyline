1588cd0ebc814ee5dd5676f00a6594b9f1004559
David Phillips <david.phillips@nikolamotor.com>
2021-02-11 10:22:37 -0700

    Update name for consistency
    Signed-off-by: David Phillips <david.phillips@nikolamotor.com>

--- crystal/flexpolyline/spec/flex_polyline_spec.cr
+++ crystal/flexpolyline/spec/flex_polyline_spec.cr
@@ -17,7 +17,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 require "spec"
-require "../src/flex_polyline.cr"
+require "../src/flexpolyline.cr"
 
 include PolylineEncoderDecoder
 
